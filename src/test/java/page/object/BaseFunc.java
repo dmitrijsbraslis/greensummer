2a349b4ed4326fe07f4b3fca07735ec5654bec1e
Dmitrijs <dmitrijs.braslis@gmail.com>
2020-08-23 15:11:13 +0300

    pageObject

---
+++ src/test/java/page/object/BaseFunc.java
@@ -0,0 +1,44 @@
+package page.object;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class BaseFunc {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private final Logger LOGGER = LogManager.getLogger(this.getClass());
+
+    public BaseFunc() {
+        System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    public void openPage(String url) {
+        LOGGER.info("Trying to open page: " + url);
+
+        if (!url.startsWith("http://") && !url.startsWith("https://")) {
+            url = "http://" + url;
+        }
+
+        driver.get(url);
+    }
+
+    public List<WebElement> findElements(By locator) {
+        return driver.findElements(locator);
+    }
+
+    public void click(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+    }
+}
---
+++ src/test/java/page/object/tests/PageObjectTests.java
@@ -0,0 +1,20 @@
+package page.object.tests;
+
+import org.junit.jupiter.api.Test;
+import page.object.BaseFunc;
+import page.object.pages.ArticlePage;
+import page.object.pages.HomePage;
+
+public class PageObjectTests {
+    @Test
+    public void pageObjectTest() {
+        BaseFunc baseFunc = new BaseFunc();
+        baseFunc.openPage("delfi.lv");
+
+        HomePage homePage = new HomePage(baseFunc);
+        String homePageTitle = homePage.getTitleById(2);
+        ArticlePage articlePage = homePage.openArticle(2);
+
+//        ArticlePage articlePage = new ArticlePage(baseFunc);
+    }
+}
